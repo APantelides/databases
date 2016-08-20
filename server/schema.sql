262d834ff5aef4a9651cab972dd2a26d40c4f889
apantelides <alex.pantelides@gmail.com>
2016-08-19 17:31:05 -0700

    Implement first 2 table schemas

--- server/schema.sql
+++ server/schema.sql
@@ -3,7 +3,22 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  createdAt datetime DEFAULT CURRENT_TIMESTAMP,
+  updatedAt datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  usernameId int,
+  message varchar (200),
+  roomId int
+);
+
+CREATE TABLE users (
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name varchar (15)
+);
+
+CREATE TABLE rooms (
+  id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name varchar (15)
 );
 
 /* Create other tables and define schemas for them here! */
